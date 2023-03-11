2272c1aba99cece5e5c2a022b363e301148b33e4
omoluabidotcom <yyahaya222@yahoo.com>
2023-03-11 22:50:09 +0100

    initial commit

---
+++ src/main/java/com/omoluabi/paystackintegrate/PaystackIntegrateApplication.java
@@ -0,0 +1,13 @@
+package com.omoluabi.paystackintegrate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaystackIntegrateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaystackIntegrateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/omoluabi/paystackintegrate/PaystackIntegrateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omoluabi.paystackintegrate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaystackIntegrateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
